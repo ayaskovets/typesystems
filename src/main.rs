f5073a095b8c02435f60b106fba8205dea0b1bf6
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2021-06-22 21:55:12 +0300

    Add license text

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2020-present, Andrei Yaskovets
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+mod lexer;
+
 fn main() {
-    
+
 }
