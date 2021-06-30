6859e79bd04a53e5eb59a863cc927417a9dfdb18
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2021-06-30 22:34:15 +0300

    Add additional stream utilities

---
+++ parser_stream/src/util.rs
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) 2021, Andrei Yaskovets
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+use super::Stream;
+
+impl<'a, T> Stream<'a, T>
+where
+    T: Clone,
+{
+    pub fn fallback<Any>(&mut self, len: usize) -> Option<Any> {
+        self.undo(len);
+        None
+    }
+
+    pub fn squash<P>(&mut self, predicate: P)
+    where
+        P: Fn(&T) -> bool,
+    {
+        loop {
+            if let Some(c) = self.next() {
+                if predicate(&c) {
+                    continue;
+                } else {
+                    self.undo(1);
+                    break;
+                }
+            }
+            break;
+        }
+    }
+
+    pub fn take_while<P>(&mut self, predicate: P) -> Vec<T>
+    where
+        P: Fn(&T) -> bool,
+    {
+        let mut taken: Vec<T> = Vec::new();
+        loop {
+            if let Some(c) = self.next() {
+                if predicate(&c) {
+                    taken.push(c);
+                    continue;
+                } else {
+                    self.undo(1);
+                    break;
+                }
+            }
+            break;
+        }
+        taken
+    }
+}
