0203f9368f15b3669a57ef2b88f487c98554b0af
Andrei Yaskovets <23223647+ayaskovets@users.noreply.github.com>
2021-08-10 23:34:23 +0300

    Initial version of expr parsing

--- src/algorithm_w/expr.rs
+++ src/algorithm_w/expr.rs
@@ -5,50 +5,124 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-pub use super::Token;
-pub use parser_stream::{Stream, Streamable};
+pub use tokenstream::*;
+
+pub use crate::Token;
 
 #[derive(PartialEq, Clone, Debug)]
-enum Expr {
+pub enum Term {
     Var(String),
-    Call(Box<Expr>, Vec<Box<Expr>>),
-    Fn(Vec<String>, Box<Expr>),
-    Let(String, Box<Expr>, Box<Expr>),
+    Call(Box<Term>, Vec<Box<Term>>),
+    Fn(Vec<String>, Box<Term>),
+    Let(String, Box<Term>, Box<Term>),
 }
 
 #[derive(PartialEq, Clone, Debug)]
-enum TypeVar {
+pub enum TypeVar {
     Unbound(i32, i32),
     Link(Box<Type>),
     Generic(i32),
 }
 
 #[derive(PartialEq, Clone, Debug)]
-enum Type {
+pub enum Type {
     Const(String),
     App(Box<Type>, Vec<Box<Type>>),
     Arrow(Vec<Type>, Box<Type>),
     Var(TypeVar),
 }
 
-#[derive(PartialEq, Clone, Debug)]
-pub enum Total {
-    Expr(Expr),
-    Type(Type),
+fn ident() -> Parser<'static, Token, String> {
+    Parser::new(|s| {
+        s.next().and_then(|t| {
+            if let Token::Ident(ident) = t {
+                Some(ident)
+            } else {
+                None
+            }
+        })
+    })
 }
 
-impl Streamable<Token> for Total {
-    #[rustfmt::skip]
-    fn from(s: &mut Stream<Token>) -> Option<Total> {
-           expr(s).or_else(
-        || r#type(s))
-    }
+fn token(t: Token) -> Parser<'static, Token, Token> {
+    satisfy(move |x| x == t)
 }
 
-fn expr(s: &mut Stream<Token>) -> Option<Total> {
-
+fn simple_term() -> Parser<'static, Token, Term> {
+    fmap(Term::Var, ident())
+        | term().between(token(Token::Lparen), token(Token::Rbracket))
+        | bind(simple_term(), |f| {
+            bind(
+                fmap(Box::new, term())
+                    .sep_by(token(Token::Comma))
+                    .between(token(Token::Lparen), token(Token::Rparen)),
+                move |args| Parser::pure(Term::Call(Box::new(f.clone()), args)),
+            )
+        })
+        | simple_term().between(token(Token::Lparen), token(Token::Rparen))
 }
 
-fn r#type(s: &mut Stream<Token>) -> Option<Total> {
+pub fn term() -> Parser<'static, Token, Term> {
+    simple_term()
+        | bind(
+            token(Token::Let) >> ident() << token(Token::Equals),
+            |ident| {
+                bind(term() << token(Token::In), move |assign| {
+                    let ident = ident.clone();
+                    bind(term(), move |term| {
+                        Parser::pure(Term::Let(
+                            ident.clone(),
+                            Box::new(assign.clone()),
+                            Box::new(term),
+                        ))
+                    })
+                })
+            },
+        )
+        | bind(
+            token(Token::Fn) >> ident().sep_by(token(Token::Spacing)) << token(Token::Arrow),
+            |idents| {
+                bind(term(), move |term| {
+                    Parser::pure(Term::Fn(idents.clone(), Box::new(term)))
+                })
+            },
+        )
+}
 
+fn simple_type() -> Parser<'static, Token, Type> {
+    fmap(Type::Const, ident())
+        | bind(simple_type(), |f| {
+            bind(
+                fmap(Box::new, r#type())
+                    .sep_by(token(Token::Comma))
+                    .between(token(Token::Lparen), token(Token::Rparen)),
+                move |args| Parser::pure(Type::App(Box::new(f.clone()), args)),
+            )
+        })
+        | r#type().between(token(Token::Lparen), token(Token::Rparen))
+}
+
+pub fn r#type() -> Parser<'static, Token, Type> {
+    simple_type()
+        | fmap(
+            |ty| Type::Arrow(vec![], Box::new(ty)),
+            (token(Token::Lparen) >> token(Token::Rparen) >> token(Token::Arrow)) >> r#type(),
+        )
+        | bind(simple_type() << token(Token::Arrow), |tl| {
+            fmap(
+                move |tr| Type::Arrow(vec![tl.clone()], Box::new(tr)),
+                r#type(),
+            )
+        })
+        | bind(
+            (r#type() << token(Token::Comma))
+                & (r#type().sep_by(token(Token::Comma)))
+                    .between(token(Token::Lparen), token(Token::Rparen)),
+            |tl| {
+                fmap(
+                    move |tr| Type::Arrow(tl.clone(), Box::new(tr)),
+                    token(Token::Arrow) >> r#type(),
+                )
+            },
+        )
 }
